51028f225a9710c678ff60d74d1477ef2f26c516
Eddie Ng <wards.ng@gmail.com>
2014-08-27 02:59:37 -0400

    Added preliminary pretty-print of dictionary and JSON. Added basic interactive prompt.

--- seatgeek.py
+++ seatgeek.py
@@ -5,8 +5,60 @@
 
 # Print text in all colors
 
-from clint.textui import colored, puts
+import sys
+import requests
+import json
+import readline
+
+from clint.textui import colored, indent, puts
+
+
+
+def pretty_dict(d):
+    def pretty(d, indent):
+        #two spaces
+        space = '  '
+        for i, (key, value) in enumerate(iter(sorted(d.iteritems()))):
+            if isinstance(value, dict):
+                print '{0}"{1}": {{'.format( space * indent, str(key))
+                pretty(value, indent+1)
+                if i == len(d)-1:
+                    print '{0}}}'.format( space * indent)
+                else:
+                    print '{0}}},'.format( space * indent)
+            else:
+                if i == len(d)-1:
+                    print '{0}"{1}": "{2}"'.format( space * indent, str(key), value)
+                else:
+                    print '{0}"{1}": "{2}",'.format( space * indent, str(key), value)
+    print '{'
+    pretty(d,indent=1)
+    print '}'
+
+def call_api_with_results(url):
+    r = requests.get(url)
+
+    if (r.status_code >= 200 and r.status_code < 300):
+        print colored.green(str(r.status_code))
+    else:
+        print colored.red(str(r.status_code))
+
+    print colored.cyan('Headers:')
+    pretty_dict(r.headers)
+
+    parsed_content = json.loads(r.content)
+    print colored.magenta('Content:')
+    print json.dumps(parsed_content, indent=2, sort_keys=True)
+
 
 if __name__ == '__main__':
-  for color in colored.COLORS:
-    puts(getattr(colored,color)('Text in {0:s}'.format(color.upper())))
+
+    print "Welcome to the SeatGeek API Explorer!"
+
+    while True:
+        in_data = raw_input(colored.yellow('>>  '))
+
+        if len(in_data) != 0:
+            print in_data
+
+            call_api_with_results("http://api.seatgeek.com/2/events?venue.state=NY")
